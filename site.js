72fb47e3f11f8c942c637916725f8a7d383e8e61
Jessica Ward <Jessicawardiom@gmail.com>
2020-08-19 17:38:00 +0100

    Added colour randomisation

--- site.js
+++ site.js
@@ -3,3 +3,32 @@ var colours = [
     ["red", "blue"],
     ["yellow", "green"]
 ];
+
+var canvas;
+var context;
+var gameProperties = {
+    height: 500,
+    width: 500,
+    colourPalette: 0
+}
+
+function init(){
+    randomiseColourPalette();
+
+    var canvas = document.getElementById("gameCanvas");
+
+    if(canvas.getContext){
+        context = canvas.getContext("2d");
+    }
+    else{
+        alert("Could not find canvas, this may be due to an unsupported browser.");
+    }
+}
+
+function randomiseColourPalette(){
+    gameProperties.colourPalette = getRandomNumber(0, colours.length);
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
